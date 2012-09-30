52768ce5df41eabf5ad4d005c8e95c52b1c115b2
Alberto De Bortoli <albertodebortoli@gmail.com>
2012-09-30 12:58:39 +0200

    Add ADBSlider to allow handle size setting (fix)

---
+++ DCControls/ADBSlider.h
@@ -0,0 +1,15 @@
+//
+//  ADBSlider.h
+//  DCControls
+//
+//  Created by Alberto De Bortoli on 9/29/12.
+//
+//
+
+#import "DCSlider.h"
+
+@interface ADBSlider : DCSlider;
+
+- (id)initWithFrame:(CGRect)frame delegate:(id <DCControlDelegate>)delegate;
+
+@end
